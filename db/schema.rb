e147e3a556b12c07045c5a506b83abf05fe236c5
Pat Dayton <daytonpe14@gmail.com>
2016-08-08 10:26:14 -0500

    Create user model and join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808143543) do
+ActiveRecord::Schema.define(version: 20160808152045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.string   "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies_users", id: false, force: :cascade do |t|
+    t.integer "user_id",  null: false
+    t.integer "movie_id", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
